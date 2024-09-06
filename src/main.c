d291599104f04fea821e990aa801469aef77f1a5
Ryan Murphy <toadgit76@gmail.com>
2024-09-06 08:51:21 -0400

    Spruced up main.c

--- src/main.c
+++ src/main.c
@@ -1,13 +1,12 @@
-
-
 #include <grrlib.h>
 
 #include <stdlib.h>
 #include <math.h>
 #include <malloc.h>
+
 #include <wiiuse/wpad.h>
 
-int main(int argc, char **argv) {
+int main(void) {
     // Initialise the Graphics & Video subsystem
     GRRLIB_Init();
 
@@ -17,16 +16,23 @@ int main(int argc, char **argv) {
 
         WPAD_ScanPads();  // Scan the Wiimotes
 
-        // If [HOME] was pressed on the first Wiimote, break out of the loop
-        if (WPAD_ButtonsDown(0) & WPAD_BUTTON_HOME)  break;
- 
-        GRRLIB_Printf(5, 25,--need to add own images for our fontt, GRRLIB_WHITE, 1, "wiisaber is a clone of beatsaber ");
+        // If [HOME] was pressed on a Wiimote, break out of the loop
+        if (WPAD_ButtonsDown(0) & WPAD_BUTTON_HOME) {
+		 break;
+	}
+
+ 	GRRLIB_Printf(5, 
+		25,
+		// --need to add own images for our font!
+		GRRLIB_WHITE, 
+		1,
+		 "wiisaber is a clone of beatsaber ");
 
          
-        GRRLIB_Render();  // Render the frame buffer to the TV
+	GRRLIB_Render();  // Render the frame buffer to the screen
     }
 
-    GRRLIB_Exit(); // Be a good boy, clear the memory allocated by GRRLIB
+    GRRLIB_Exit(); 	// Be a good boy, clear the memory allocated by GRRLIB
 
     exit(0);  // Use exit() to exit a program, do not use 'return' from main()
 }
