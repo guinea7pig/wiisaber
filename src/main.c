3f9a119fa88851c3f3c6ecec0cbce2c09e559c49
ToadRage <153384304+toadragethe5th@users.noreply.github.com>
2024-09-10 10:06:49 -0400

    Update main.c

--- src/main.c
+++ src/main.c
@@ -13,27 +13,28 @@
 #include <wiiuse/wpad.h>
 
 #define WHITE 0xFFFF
-// #include "imgload.h"
+#include "imgload.h"
 
 int main(void) {
-    // Initialise the Graphics & Video subsystem
+    // Initialise the Graphics & Video subsystems
     GRRLIB_Init();
 
     WPAD_Init();
 	
-    // load_img(); // called from imgload.h
+    load_img(); // called from imgload.h
 
 
     while(1) {
 
         WPAD_ScanPads();  // Scan the Wiimotes
 
-        // If [HOME] was pressed on a Wiimote, break out of the loop
+        // If HOME was pressed on a Wiimote, break out of the loop
         if (WPAD_ButtonsDown(0) & WPAD_BUTTON_HOME) {
 		 exit(1);
 	}
 	    
-	GRRLIB_texIMG *BMfont3_png = GRRLIB_LoadTextureFromFile("BMfont3.png");
+	/* image loading is handled from "imgload.h" */
+	
 	GRRLIB_SetBackgroundColour(0000000f); 					//this might be wrong 
 	
  	GRRLIB_Printf(5, 
@@ -52,9 +53,11 @@ int main(void) {
 
          
 	GRRLIB_Render();  // Render the frame buffer to the screen
+	    
     }
 
     GRRLIB_Exit(); 	// Be a good boy, clear the memory allocated by GRRLIB
 
     exit(0);  // Use exit() to exit a program, do not use 'return' from main()
+	
 }
