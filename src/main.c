ec3b90a3a0998ba45e07f3343cd0bffe79500838
ToadRage <153384304+toadragethe5th@users.noreply.github.com>
2024-09-16 08:03:23 -0400

    Update main.c

--- src/main.c
+++ src/main.c
@@ -13,52 +13,54 @@
 #include <wiiuse/wpad.h>
 
 #define WHITE 0xFFFF
+
 #include "imgload.h"  //do in need to put my font png there
+		      // I would do that if I were you. - Toad
 
 int main(void) {
-    // Initialise the Graphics & Video subsystems
-    GRRLIB_Init();
+    	// Initialise the Graphics & Video subsystems
+	GRRLIB_Init();
 
-    WPAD_Init();
+	WPAD_Init();
 	
-    load_img(); // called from imgload.h
+    	load_img(); // called from imgload.h
 
-   GRRLIB_texImg *tex_BMfont4 = GRRLIB_LoadTexture(//add own texture here);
+   	GRRLIB_texImg *tex_BMfont4 = GRRLIB_LoadTexture(/* add own texture here */);
 
 
-    while(1) {
+    	while(1) {
 
-        WPAD_ScanPads();  // Scan the Wiimotes
+        	WPAD_ScanPads();  // Scan the Wiimotes
 
         // If HOME was pressed on a Wiimote, break out of the loop
-        if (WPAD_ButtonsDown(0) & WPAD_BUTTON_HOME) {
-		 exit(1);
-	}
+        	if (WPAD_ButtonsDown(0) & WPAD_BUTTON_HOME) {
+		 	exit(1);
+		}
 	    
 	/* image loading is handled from "imgload.h" */
 	
-	GRRLIB_SetBackgroundColour(0x000000FF); 		//should be rgba 			//this might be wrong 
+		GRRLIB_SetBackgroundColour(0x000000FF); 		//should be rgba
 	
- 	GRRLIB_Printf(5, 
-		25,
-		BMfont3.png,
-		WHITE, 
-		1,
-		 "wiisaber is a clone of beatsaber ");
+ 		GRRLIB_Printf(5, 
+			25,
+			tex_BMfont3,
+			WHITE, 
+			1,
+		 	"wiisaber is a clone of beatsaber ");
 
-	    GRRLIB_Printf(5, 
-		0,
-		BMfont3.png
-		WHITE, 
-		1,
-		 "© toadrage and guinea7pig 2024");
+	    	GRRLIB_Printf(5, 
+			0,
+			tex_BMfont3,
+			WHITE, 
+			1,
+			"© toadrage and guinea7pig 2024"); // Might replace this with a license boilerplate.
 
          
-	GRRLIB_Render();  // Render the frame buffer to the screen
+		GRRLIB_Render();  // Render the frame buffer to the screen
 	    
-    }
+   	}
 
-    GRRLIB_Exit(); 	// Be a good boy, clear the memory allocated by GRRLIB
+    	GRRLIB_Exit(); 	// Be a good boy, clear the memory allocated by GRRLIB
 
     exit(0);  // Use exit() to exit a program, do not use 'return' from main()
 	
