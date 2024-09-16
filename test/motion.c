a2f5c6f69f2d3db7fc71e6d6cf6bbc561b55089f
Ryan Murphy <toadgit76@gmail.com>
2024-09-16 13:18:46 -0400

    Polished motion code.

--- test/motion.c
+++ test/motion.c
@@ -1,2 +1,13 @@
 //for the motion controls
 #include <wiiuse/wpad.h>
+#include <wiiuse/wiiuse.h>
+
+#include <stdint.h>
+#include <stdio.h>
+
+void motion(u32 channel)
+{
+	WPADData *data = WPAD_Data(channel);
+	orient_t orientation = data->orient;
+	return orient;
+}
