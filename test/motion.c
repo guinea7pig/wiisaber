79b747d0579ae416e35f5ce040048309c9487215
ToadRage <153384304+toadragethe5th@users.noreply.github.com>
2024-09-18 10:57:43 -0400

    Update motion.c

--- test/motion.c
+++ test/motion.c
@@ -7,15 +7,19 @@
 #include <stdint.h>
 #include <stdio.h>
 // how would we put thes values in a header file
-void motion(u32 channel)
+
+orient_t* motion(u32 channel)
 {
 	WPADData *data = WPAD_Data(channel);
-	orient_t orientation = data->orient;
-	return orient;
+	orient_t orient = data->orient;
+	return &orient;
 }
-void testmotion()
+
+void testmotion(u32 channel)
 {
-//make it print the yaw,rool and pitch vales to test
-//hopefully grrlib supports yaw,rool and pitch for 3d objects
-printf(orient)//me thinks this is how it works
+	orient_t* orient = motion(channel);
+	//make it print the yaw,rool and pitch vales to test
+	//hopefully grrlib supports yaw,rool and pitch for 3d objects
+	printf(orient->yaw);
+	printf(orient->pitch);			//me thinks this is how it works
 }
