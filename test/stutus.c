281e0db82437f21cdf7bb10a5f1140dbe37a2091
ToadRage <153384304+toadragethe5th@users.noreply.github.com>
2024-09-18 13:15:06 -0400

    Update stutus.c

--- test/stutus.c
+++ test/stutus.c
@@ -7,5 +7,5 @@ void handle_ctrl_status(struct wiimote_t* wm) {
 	printf("speaker:         %i\n", WIIUSE_USING_SPEAKER(wm));
 	printf("ir:              %i\n", WIIUSE_USING_IR(wm));
 	printf("leds:            %i %i %i %i\n", WIIUSE_IS_LED_SET(wm, 1), WIIUSE_IS_LED_SET(wm, 2), WIIUSE_IS_LED_SET(wm, 3), WIIUSE_IS_LED_SET(wm, 4));
-	printf("battery:         %f %%\n", wm->battery_level);\
+	printf("battery:         %f %%\n", wm->battery_level);
 }
