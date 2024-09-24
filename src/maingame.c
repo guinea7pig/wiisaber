9016908805a5732abc24053bec4889c2216377b5
guinea7pig <145406121+guinea7pig@users.noreply.github.com>
2024-09-24 12:37:39 -0400

    Create maingame.c

---
+++ src/maingame.c
@@ -0,0 +1,16 @@
+#incluse grrlib.h
+#incude motion.h
+#include input.h
+int yaw = 0
+int roll=0
+int pitch=0
+void main()
+{
+GRRLIB_ObjectView(480,50,10,)
+}
+
+void conversion()
+{
+}
+
+//converts rool yaw and pitch to angle valuses
