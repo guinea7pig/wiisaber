879d6c6fa4614dcfd38642e517a3420c1d3ebf5b
guinea7pig <145406121+guinea7pig@users.noreply.github.com>
2024-09-27 13:27:14 -0400

    Update maingame.c

--- src/maingame.c
+++ src/maingame.c
@@ -8,12 +8,20 @@ int sabery=0
 int saberxrot=0
 int sbaeryrot
 int saberzrot
+bool paused = false
+
 void grlib_init()//using grrlib for 2d elments 
 {
 Grrlib_Init()
 }
 
-
+void debug()
+{
+GRRLIB_Printf(50,0,,fffff,1,saberx)//need to include image this applys to all
+GRRLIB_Printf(50,10,,ffff,1,sabery)
+GRRLIB_Printf(50,,20,,ffff,saberxrot)
+GRRLIB_Printf(
+}
 
 
 void main()
@@ -21,13 +29,12 @@ void main()
 render()
 }
 
-void rectangle()
-{
-GRRLIB_DrawCube(1,true,0000ff);
+void saber()
+//draws saber 
+GRRLIB_DrawCube(1,true,FF2D00);
 GRRLIB_ObjectViewScale(2,1,1)
 GRRLIB_ObjectVeiwTrans(saberx,sabery,saberz)//has something to do with tranlating it to screen
 GRRLIB_OBJECTVeiwRotate(saberxrot,saberyrot,saberzrot)//wimote values are in yaw picth and roll witch is annoying
-   
 }
 
 void render()
@@ -58,12 +65,15 @@ saberyrot=saberyrot+1
 }
 if(ButtonsDown & WPAD_BUTTON_DOWN()
 {
-saberyrot=saberyrot-1
-}
+saberyrot=saberyroty-1
 }
 void pause()
 {
-
+puased= true //need to ad something where it freezes game 
+if(ButtonsDown & WPAD_BUTTON_HOME && pause=true)
+{
+GRLIB_EXIT()
+exit(0)
 }
 }
-
+}
