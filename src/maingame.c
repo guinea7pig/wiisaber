d5142b92b0d4d82f9ff8404b6ad45f6b6da7afe4
guinea7pig <145406121+guinea7pig@users.noreply.github.com>
2024-09-27 09:20:23 -0400

    Update maingame.c

--- src/maingame.c
+++ src/maingame.c
@@ -27,7 +27,7 @@ GRRLIB_DrawCube(1,true,0000ff);
 GRRLIB_ObjectViewScale(2,1,1)
 GRRLIB_ObjectVeiwTrans(saberx,sabery,saberz)//has something to do with tranlating it to screen
 GRRLIB_OBJECTVeiwRotate(saberxrot,saberyrot,saberzrot)//wimote values are in yaw picth and roll witch is annoying
-
+   
 }
 
 void render()
@@ -37,13 +37,22 @@ GRRLIB_Render()
 
 while(1)
 {
+u32 buttonsDown = WPAD_ButtonsDown();
 WPAD_SCANPADS()//forget how to format this
  
-if(WPAD_ButtonsDownW $ PAD_BUTTON_HOME)
+if(ButtonsDown & WPAD_BUTTON_HOME)
 {
 pause()
 }
-
+if(ButtonsDown & WPAD_BUTTON_RIGHT)   //making sure it actlly it actlully roatess befor motion controls 
+{
+saberxrot=saberxrot+1
+}
+if(ButtosDown & WPAD_BUTTON_LEFT)
+{
+saberxtot=saberxrot-1
+}
+}
 void pause()
 {
 
