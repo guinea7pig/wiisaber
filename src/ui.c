5a1553b918cb1104be1501c7bb81696e78646a12
guinea7pig <145406121+guinea7pig@users.noreply.github.com>
2024-10-01 12:36:57 -0400

    Add files via upload

---
+++ src/ui.c
@@ -0,0 +1,29 @@
+#include grrlub.h
+
+int score=0
+int level_comp=0
+bool level_fin=false
+//runs over main games 
+void score()
+{
+        //when stuff colisds it ups the score 
+
+}
+
+
+void level_complete()
+{
+    //etheir going to decide how o want to do that 
+        if(level_comp==100)
+        {
+            level_fin=true     
+            resaults()
+        }
+}
+
+void resaults()
+{
+    //prints the score and more
+    GRRLIB_Printf()) 
+
+}
