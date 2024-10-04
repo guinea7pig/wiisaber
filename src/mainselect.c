7b0145f19c05fc35be8ea2fbb3fbb45abbebd076
guinea7pig <145406121+guinea7pig@users.noreply.github.com>
2024-10-04 10:57:17 -0400

    Update mainselect.c

--- src/mainselect.c
+++ src/mainselect.c
@@ -2,3 +2,5 @@
 //really neet find a way to orginize the programs 
 #include <wiiuse/wpad.h>
 printf("mainmenu stablility good, press a to start")
+orintf("maingame, stabity proably not good and needs better coding, press 2")
+printf("main, should work exstreamly well, press 1")
