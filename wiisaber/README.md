fc1976b6988e29e04fac4a4717178a6dacb6dc50
guinea7pig <145406121+guinea7pig@users.noreply.github.com>
2025-02-11 09:54:14 -0500

    Add files via upload

---
+++ wiisaber/README.md
@@ -0,0 +1,3 @@
+a wii port of beatsaber. 
+ is the side  project for siiiaber
+reguired to compile  wiiuse and grrlib.
---
+++ wiisaber/source/ui.c
@@ -0,0 +1,28 @@
+#include <grrlib.h>
+int score=0;
+int level_comp=0;
+bool level_fin=false;
+//runs over main games
+void scoree()
+{
+        //when stuff colisds it ups the score;
+
+}
+
+
+void level_complete()
+{
+    //etheir going to decide how o want to do that;
+        if(level_comp==100)
+        {
+            level_fin=true;     
+            resaults();
+        }
+}
+
+void resaults()
+{
+    //prints the score and more
+    //need to ad score here
+
+}
